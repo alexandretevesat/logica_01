453e1fb4bca06c0dd2a4e96eb1fbc21a77c8e72c
unknown <alexandreteves0834@digitalcollege.internal>
2025-09-30 14:39:56 -0300

    durante a aula

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 alert("Bem Vindo!");
 
 var nome;
-
 nome = "Alex";
-
 console.log(nome);
 
 var frase;
 frase = "meu nome é Alexandre 1979";
 console.log(frase);
+console.log(typeof frase);
+
+var idade;
+idade = 29.11;
+console.log(idade);
+
+var ativo;
+ativo = false;
+console.log(ativo);
+
+var contem;
+contem = null;
+console.log(contem);
+
+var definido;
+
+console.log(4 + 4);
+console.log(4 - 4);
+console.log(4 * 4);
+console.log(4 / 4);
+console.log(4 % 4);
+console.log(4 ** 4);
+
+console.log(10 > 5); // maior que
+console.log(10 < 5); // menor que
+console.log(10 == 10); // igual (, quando um number se iguala a string)
+console.log(10 === 10); //estrita igualdade
+
+var numero = 10; // atribuição
+numero += 3; // atribuição com adição de valor +1
+numero -= 1; // atribuição com subtração
+numero *= 10;
+numero /= 2;
+console.log(numero);
